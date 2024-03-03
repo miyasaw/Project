e0bbe14b204cc4f66f8faff1fb7914b4ab450c92
miyasaw <miyabecky@icloud.com>
2024-03-04 02:37:48 +0900

    検索機能の追加

--- app/forms/search_articles_form.rb
+++ app/forms/search_articles_form.rb
@@ -2,8 +2,11 @@ class SearchArticlesForm
   include ActiveModel::Model
   include ActiveModel::Attributes
 
+  attribute :author_id, :integer
   attribute :category_id, :integer
+  attribute :tag_id, :integer
   attribute :title, :string
+  attribute :body, :string
 
   def search
     relation = Article.distinct
@@ -12,6 +15,11 @@ class SearchArticlesForm
     title_words.each do |word|
       relation = relation.title_contain(word)
     end
+    relation = relation.by_author(author_id) if author_id.present?
+    relation = relation.by_tag(tag_id) if tag_id.present?
+    body_words.each do |word|
+      relation = relation.body_contain(word)
+    end
     relation
   end
 
@@ -19,5 +27,9 @@ class SearchArticlesForm
 
   def title_words
     title.present? ? title.split(nil) : []
+  end
+
+  def body_words
+    body.present? ? body.split(nil) : []
   end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -61,8 +61,11 @@ class Article < ApplicationRecord
 
   scope :viewable, -> { published.where('published_at < ?', Time.current) }
   scope :new_arrivals, -> { viewable.order(published_at: :desc) }
+  scope :by_author, ->(author_id) { where(author_id: author_id) }
   scope :by_category, ->(category_id) { where(category_id: category_id) }
+  scope :by_tag, ->(tag_id) { joins(:article_tags).where(article_tags: { tag_id: tag_id }) }
   scope :title_contain, ->(word) { where('title LIKE ?', "%#{word}%") }
+  scope :body_contain, ->(word) { joins(:sentences).where('sentences.body LIKE ?', "%#{word}%") }
   scope :past_published, -> { where('published_at <= ?', Time.current) }
 
   def build_body(controller)
